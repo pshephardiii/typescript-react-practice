7974f6d19da7751fe3654f3b204b14959c118fd0
Paul Shephard <paulshephard598@gmail.com>
2024-07-23 12:18:37 -0500

    course goal and header components

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,8 +1,14 @@
 import CourseGoal from './components/CourseGoal.tsx'
+import Header from './components/Header.tsx'
+import goalsImg from './assets/goals.jpg'
 
 export default function App() {
   return (
     <main>
+      <Header 
+      image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal 
       title="Learn React + TS"
       description="Learn it from the groun up"/>
--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,10 +1,13 @@
-export default function CourseGoal({ 
-    title, 
-    description
-}: {
-    title: string; 
+// import { type ReactNode } from 'react'
+// ReactNode type should be set for children of object
+// also there's a type PropsWithChildren
+
+type CourseGoalProps = {
+    title: string;
     description: string
-}) {
+}
+
+export default function CourseGoal({ title, description }: CourseGoalProps ) {
     return (
         <article>
             <div>
