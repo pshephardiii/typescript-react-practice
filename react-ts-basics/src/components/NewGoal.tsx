4250b9525e5bcbe1dd25fab52b63959ba5a242ab
Paul Shephard <paulshephard598@gmail.com>
2024-07-23 13:27:22 -0500

    added input form functionality

--- react-ts-basics/src/components/NewGoal.tsx
+++ react-ts-basics/src/components/NewGoal.tsx
@@ -1,20 +1,35 @@
-import { type FormEvent } from 'react'
+import { useRef, type FormEvent } from 'react'
 
-export default function NewGoal() {
+type NewGoalProps = {
+    onAddGoal: (goal: string, summary: string) => void
+}
 
-    function handleSubmit(event: FormEvent) {
+export default function NewGoal({onAddGoal}: NewGoalProps) {
+
+    // HTML Input Element is a typeScript type
+    const goal = useRef<HTMLInputElement>(null)
+    const summary = useRef<HTMLInputElement>(null)
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
+        // exclamation point makes clear that the value will never be null... but need to be certain, because app will crash if it is null
+        const enteredGoal = goal.current!.value
+        const enteredSummary = summary.current!.value
+
+        // resets form inputs on submit
+        event.currentTarget.reset()
+        onAddGoal(enteredGoal, enteredSummary)
     }
 
     return(
         <form onSubmit={handleSubmit}>
             <p>
                 <label>Your goal</label>
-                <input id="goal" type="text" />
+                <input id="goal" type="text" ref={goal} />
             </p>
             <p>
                 <label>Short Summary</label>
-                <input id="summary" type="text" />
+                <input id="summary" type="text" ref={summary} />
             </p>
             <p>
                 <button>Add Goal</button>
