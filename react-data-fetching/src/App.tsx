a80be10f94d2cd37e9341b214f49c5f1758b6911
Paul Shephard <paulshephard598@gmail.com>
2024-07-24 13:51:48 -0500

    added handling for error and loading states

--- react-data-fetching/src/App.tsx
+++ react-data-fetching/src/App.tsx
@@ -2,6 +2,7 @@ import { type ReactNode, useState, useEffect } from 'react';
 import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
 import { get } from './util/http.ts'
 import fetchingImg from './assets/data-fetching.png'
+import ErrorMessage from './components/ErrorMessage.tsx';
 
 // getting this from dummy data site
 type RawDataBlogPost = {
@@ -13,24 +14,37 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
+  // to give users a loading screen
+  const [isFetching, setIsFetching] = useState(false)
+  // for error state
+  const [error, setError] = useState<string>()
 
   // to prevent infinite loop
   useEffect(() => {
     async function fetchPosts() {
-      // fetching dummy data
-      const data = (await get(
-        'http://jsonplaceholder.typicode.com/posts'
-      )) as RawDataBlogPost[]
+      setIsFetching(true)
+      try {
+         // fetching dummy data
+        const data = (await get(
+          'http://jsonplaceholder.typicode.com/posts'
+        )) as RawDataBlogPost[]
 
-      const blogPosts: BlogPost[] = data.map(rawPost => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+        const blogPosts: BlogPost[] = data.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          }
+        })
+
+        setFetchedPosts(blogPosts)
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message)
         }
-      })
+      }
 
-      setFetchedPosts(blogPosts)
+      setIsFetching(false)
     }
 
     fetchPosts()
@@ -38,8 +52,16 @@ function App() {
 
   let content: ReactNode
 
+  if (error) {
+    content = <ErrorMessage text={error}></ErrorMessage>
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>
   }
   
   return (
